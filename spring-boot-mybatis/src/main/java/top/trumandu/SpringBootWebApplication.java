eb1ab51c60760bc4ab4cb25340f9b02baea0467a
TrumanDu <truman.p.du@newegg.com>
2021-08-14 09:51:33 +0800

    feat: 重构目录结构 重构目录结构 升级spring版本

--- spring-boot-mybatis/src/main/java/top/trumandu/SpringBootWebApplication.java
+++ spring-boot-mybatis/src/main/java/top/trumandu/SpringBootWebApplication.java
@@ -3,6 +3,9 @@ package top.trumandu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Truman.P.Du
+ */
 @SpringBootApplication
 public class SpringBootWebApplication {
 
