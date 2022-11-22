e8cefb3a56793c0fa0d91dda1ed981727fa70c75
trumandu <1753177225@qq.com>
2022-11-22 21:54:21 +0800

    refactor: 重构web项目

--- spring-boot-web/src/main/java/top/trumandu/common/base/BaseController.java
+++ spring-boot-web/src/main/java/top/trumandu/common/base/BaseController.java
@@ -1,25 +1,26 @@
 package top.trumandu.common.base;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
-import top.trumandu.common.domain.RestResult;
 import top.trumandu.common.domain.SessionAttr;
 import top.trumandu.common.domain.UserInfo;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import java.util.Objects;
 
 /**
  * @author Truman.P.Du
  * @date 2020/04/25
  * @description
  */
+@SuppressWarnings("unused")
 public class BaseController {
 
 
     public UserInfo getCurrentUser() {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes()))
                 .getRequest();
         HttpSession session = request.getSession();
         UserInfo userInfo = null;
@@ -31,41 +32,4 @@ public class BaseController {
         return userInfo;
     }
 
-    public RestResult success() {
-        return RestResult.success();
-    }
-
-    public RestResult success(Object data) {
-        return RestResult.success(data);
-    }
-
-
-    /**
-     * 客户端参数错误
-     *
-     * @param message
-     * @return
-     */
-    public RestResult failure(String message) {
-        return RestResult.failure(message);
-    }
-
-    /**
-     * 服务器内部错误
-     *
-     * @param message
-     * @return
-     */
-    public RestResult error(String message) {
-        return RestResult.error(message);
-    }
-
-    /**
-     * 未认证（签名错误）
-     *
-     * @return
-     */
-    public RestResult unauthorized() {
-        return RestResult.unauthorized();
-    }
 }
