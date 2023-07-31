cd2f2589696cd854bc784e838f3bd5df2f23c292
trumandu <1753177225@qq.com>
2023-07-31 16:16:24 +0800

    feat: add example smart rest

---
+++ example-smart-rest/src/main/java/org/example/Application.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Truman.P.Du
+ * @date 2023/07/26
+ * @description
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
